e1327480ccb3e12512ba2e58b70d952fd862c2be
Volodymyr.Myshura <Volodymyr.Myshura@dreamscapenetworks.com>
2021-04-18 16:45:29 +0300

    Setup redux store

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+const initialState = {};
+
+const middlewares = [thunk];
+
+const composedEnhancers = applyMiddleware(...middlewares);
+
+const reducers = {};
+
+const rootReducer = combineReducers({
+  ...reducers,
+});
+
+const store = createStore(rootReducer, initialState, composedEnhancers);
+
+export default store;
