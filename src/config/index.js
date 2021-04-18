28ea9a6aba3457907b4b84407ee357d55397fb88
Volodymyr.Myshura <Volodymyr.Myshura@dreamscapenetworks.com>
2021-04-18 19:39:24 +0300

    Implement cache and cancel request functionlity
    - create variable for changing cache limit

--- src/config/index.js
+++ src/config/index.js
@@ -3,3 +3,5 @@ export const BACKEND_URL = 'https://api.github.com';
 export const REQUEST_TIME_DELAY = 300;
 export const ITEMS_PER_PAGE = 30;
 export const DEFAULT_SORT = 'stars';
+
+export const LIMIT_CACHE_ITEMS = 10;
--- src/services/reposService.js
+++ src/services/reposService.js
@@ -1,4 +1,13 @@
-import { BACKEND_URL, ITEMS_PER_PAGE, DEFAULT_SORT } from 'config/index';
+import {
+  BACKEND_URL,
+  ITEMS_PER_PAGE,
+  DEFAULT_SORT,
+  LIMIT_CACHE_ITEMS,
+} from 'config/index';
+
+const cache = {};
+
+let cancelController = null;
 
 export const getRepositories = async ({
   input,
@@ -8,9 +17,31 @@ export const getRepositories = async ({
 }) => {
   const url = `${BACKEND_URL}/search/repositories?q=${input}&per_page=${ITEMS_PER_PAGE}&page=${page}&order=${dir}&sort=${sort}`;
 
-  const response = await fetch(url);
+  if (cache[url]) {
+    return cache[url];
+  }
+
+  if (cancelController) {
+    cancelController.abort();
+  }
+
+  const controller = new AbortController();
+
+  cancelController = controller;
+
+  const response = await fetch(url, { signal: controller.signal });
 
   const { items, total_count: totalCount } = await response.json();
+
+  cancelController = null;
+
+  if (Object.keys(cache).length > LIMIT_CACHE_ITEMS) {
+    for (const prop of Object.getOwnPropertyNames(cache)) {
+      delete cache[prop];
+    }
+  }
+
+  cache[url] = { items, totalCount };
 
   return { items, totalCount };
 };
